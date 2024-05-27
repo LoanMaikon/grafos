002136a12c06bf3302cd5ea9ce5a345adc724236
Vinicius Evair <ves22@inf.ufpr.br>
2024-05-26 21:40:31 -0300

    [ADD] Some graph functions
    The graph part is long from being functional. Just a bunch of testing stuff right now

--- rainhas.c
+++ rainhas.c
@@ -1,6 +1,103 @@
 #include "rainhas.h"
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define BOARD_SLOTS (n * n) // Board size will always be n^2.
+#define ADD 1
+#define REM 0
+
+// Só para eu lembrar depois:
+//                              n - 1 da linha horizontal
+//                              n - 1 da linha vertical
+//                              n - 1 + 2*(min(i, j)) da diagonal
+//                              total: 3(n -1) + 2*(min(i, j)) para cada casa
+//                              maior valor da diagonal acontece no centro
+//                              o centro ocorre em ceil(n/2)
+//                              preciso alocar 3(n - 1) + n = 4n - 3
+
+struct graph_t {
+    unsigned int **adjacency;
+    unsigned int size;
+};
+
+struct set_t {
+    unsigned int size;
+    unsigned int *data;
+};
+
+struct graph_t* create_graph(unsigned int n) {
+    int i;
+    struct graph_t* g;
+
+    if(! (g = malloc(sizeof(struct graph_t)))) {
+        fprintf(stderr, "Erro ao alocar memória");
+        return NULL;
+    }
+    g->size = n;
+
+    if(! (g->adjacency = calloc(BOARD_SLOTS, sizeof(int*)))) {
+        fprintf(stderr, "Erro ao alocar memória");
+        return NULL;
+    }
+    for (i = 0; i < BOARD_SLOTS; i++)
+        if(! (g->adjacency[i] = calloc(BOARD_SLOTS, sizeof(int)))) {
+            fprintf(stderr, "Erro ao alocar memória");
+            return NULL;
+        }
+
+    return g;
+}
+
+struct set_t* create_set(unsigned int n) {
+    struct set_t *s;
+    if(! (s = malloc(sizeof(struct set_t)))) {
+        fprintf(stderr, "Erro ao alocar memória");
+        return NULL;
+    }
+    if(! (s->data = calloc(BOARD_SLOTS, sizeof(int)))) {
+        fprintf(stderr, "Erro ao alocar memória");
+        return NULL;
+    }
+    s->size = 0;
+
+    return s;
+}
+
+int get_index_from_position(int board_size, casa c) {
+    return board_size * c.linha + c.coluna;
+}
+
+void update_edge(struct graph_t *graph, casa c1, casa c2, int operation) {
+    int v1 = get_index_from_position(graph->size, c1);
+    int v2 = get_index_from_position(graph->size, c2);
+    graph->adjacency[v1][v2] = operation;
+    graph->adjacency[v2][v1] = operation;
+}
+
+void get_position_from_index(int index, int board_size, casa *c) {
+    c->linha = index / board_size;
+    c->coluna = index % board_size;
+}
+
+void print_graph(struct graph_t *g) {
+    int i, j;
+    int n = g->size;
+
+    for (i = 0; i < BOARD_SLOTS; i++) {
+        for(j = 0; j < BOARD_SLOTS; j++)
+            printf("%d ", g->adjacency[i][j]);
+        printf("\n");
+    }
+}
+
+struct set_t* append(struct set_t *s, casa v) {
+}
+
+struct set_t* remove_neighbours(struct graph_t *g, struct set_t *s, casa v) {
+    return NULL;
+}
 
 
 int isValid(unsigned int *t, unsigned int r, unsigned int j, casa *c, unsigned int k);
