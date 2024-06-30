c5da9faaa3fc22b36c31334e3a09b04c43668e14
Vinicius Evair <ves22@inf.ufpr.br>
2024-06-29 21:31:11 -0300

    [FIX] rainhas_ci properly working now
    Theres a lot of cleanup to do! Unused functions and optimizations in some

--- rainhas.c
+++ rainhas.c
@@ -12,7 +12,9 @@
 #define FALSE 0
 
 //-------------------------- Parte backtracking -------------------------------
-int isValid(unsigned int *t, unsigned int r, unsigned int j, casa *c, unsigned int k) {
+int
+isValid (unsigned int *t, unsigned int r, unsigned int j, casa *c,
+         unsigned int k) {
     for (unsigned int i = 0; i < k; i++) {
         if (c[i].linha == r + 1 && c[i].coluna == j + 1) {
             return FALSE;
@@ -31,7 +33,10 @@ int isValid(unsigned int *t, unsigned int r, unsigned int j, casa *c, unsigned i
     return TRUE;
 }
 
-void posicionarRainhas(unsigned int *t, unsigned int n, unsigned int r, casa *c, unsigned int k, unsigned int *bestSolution, unsigned int *maxRainhas) {
+void
+posicionarRainhas (unsigned int *t, unsigned int n, unsigned int r, casa *c,
+                   unsigned int k, unsigned int *bestSolution,
+                   unsigned int *maxRainhas) {
     if (r == n) {
         unsigned int count = 0;
         for (unsigned int i = 0; i < n; i++) {
@@ -56,18 +61,17 @@ void posicionarRainhas(unsigned int *t, unsigned int n, unsigned int r, casa *c,
 
     unsigned int cont = 0;
     for (unsigned int j = 0; j < n; j++) {
-        if (isValid(t, r, j, c, k)) {
+        if (isValid (t, r, j, c, k)) {
             t[r] = j + 1;
-            posicionarRainhas(t, n, r + 1, c, k, bestSolution, maxRainhas);
-        }
-        else {
+            posicionarRainhas (t, n, r + 1, c, k, bestSolution, maxRainhas);
+        } else {
             cont++;
         }
     }
 
     if (cont == n) {
         t[r] = 0;
-        posicionarRainhas(t, n, r + 1, c, k, bestSolution, maxRainhas);
+        posicionarRainhas (t, n, r + 1, c, k, bestSolution, maxRainhas);
     }
 }
 
@@ -84,19 +88,21 @@ void posicionarRainhas(unsigned int *t, unsigned int n, unsigned int r, casa *c,
 //      r[i] = 0     indica que não há rainha nenhuma na linha i+1
 //
 // devolve r
-unsigned int* rainhas_bt(unsigned int n, unsigned int k, casa *c, unsigned int *r) {
+unsigned int *
+rainhas_bt (unsigned int n, unsigned int k, casa *c, unsigned int *r) {
     unsigned int maxRainhas = 0;
-    unsigned int *t = (unsigned int*) calloc(n, sizeof(unsigned int));
-    unsigned int *bestSolution = (unsigned int*) calloc(n, sizeof(unsigned int));
+    unsigned int *t = (unsigned int *)calloc (n, sizeof (unsigned int));
+    unsigned int *bestSolution =
+        (unsigned int *)calloc (n, sizeof (unsigned int));
 
-    posicionarRainhas(t, n, 0, c, k, bestSolution, &maxRainhas);
+    posicionarRainhas (t, n, 0, c, k, bestSolution, &maxRainhas);
 
     for (unsigned int i = 0; i < n; i++) {
         r[i] = bestSolution[i];
     }
 
-    free(t);
-    free(bestSolution);
+    free (t);
+    free (bestSolution);
 
     return r;
 }
@@ -112,29 +118,29 @@ struct graph_t {
 // Pretendo depois mudar para uma tabela hash também. Vai acelerar as coisas!
 struct set_t {
     unsigned int elem_count; // Guarda a quantidade de elementos no conjunto.
-    unsigned int capacity; // Guarda o tamanho do tabuleiro
+    unsigned int capacity;   // Guarda o tamanho do conjunto
     unsigned int *data;
 };
 
 // Aloca espaço para um grafo (matriz de adjacência).
-struct graph_t*
-create_graph(unsigned int n) {
+struct graph_t *
+create_graph (unsigned int n) {
     int i;
     struct graph_t *g;
 
-    if (!(g = malloc(sizeof(struct graph_t)))) {
-        fprintf(stderr, "Erro ao alocar memória.\n");
+    if (!(g = malloc (sizeof (struct graph_t)))) {
+        fprintf (stderr, "Erro ao alocar memória.\n");
         return NULL;
     }
     g->size = n;
 
-    if (!(g->adjacency = calloc(BOARD_SQUARES, sizeof(int *)))) {
-        fprintf(stderr, "Erro ao alocar memória.\n");
+    if (!(g->adjacency = calloc (BOARD_SQUARES, sizeof (int *)))) {
+        fprintf (stderr, "Erro ao alocar memória.\n");
         return NULL;
     }
     for (i = 0; i < BOARD_SQUARES; i++)
-        if (!(g->adjacency[i] = calloc(BOARD_SQUARES, sizeof(int)))) {
-            fprintf(stderr, "Erro ao alocar memória.\n");
+        if (!(g->adjacency[i] = calloc (BOARD_SQUARES, sizeof (int)))) {
+            fprintf (stderr, "Erro ao alocar memória.\n");
             return NULL;
         }
 
@@ -142,31 +148,52 @@ create_graph(unsigned int n) {
 }
 
 // Aloca espaço para um conjunto.
-struct set_t*
-create_set(unsigned int n) {
+struct set_t *
+create_set (unsigned int n) {
     struct set_t *s;
-    if (!(s = malloc(sizeof(struct set_t)))) {
-        fprintf(stderr, "Erro ao alocar memória.\n");
+    if (!(s = malloc (sizeof (struct set_t)))) {
+        fprintf (stderr, "Erro ao alocar memória.\n");
         return NULL;
     }
-    if (!(s->data = calloc(BOARD_SQUARES, sizeof(int)))) {
-        fprintf(stderr, "Erro ao alocar memória.\n");
+    if (!(s->data = calloc (n, sizeof (int)))) {
+        fprintf (stderr, "Erro ao alocar memória.\n");
         return NULL;
     }
+    s->capacity = n;
     s->elem_count = 0;
 
     return s;
 }
 
+struct set_t *
+copy_set (struct set_t *original) {
+    if (original == NULL)
+        return NULL;
+
+    struct set_t *copy = create_set (original->capacity);
+    copy->elem_count = original->elem_count;
+    memcpy (copy->data, original->data, original->capacity * sizeof (int));
+
+    return copy;
+}
+
+void
+delete_set (struct set_t *s) {
+    if (s) {
+        free (s->data);
+        free (s);
+    }
+}
+
 // Transforma uma coordenada do tabuleiro em índice do vetor.
 int
-get_index_from_position(int board_size, casa c) {
+get_index_from_position (unsigned int board_size, casa c) {
     return board_size * c.linha + c.coluna;
 }
 
 // Transforma um índice do vetor em coordenada do tabuleiro.
 casa
-get_position_from_index(int index, int board_size) {
+get_position_from_index (unsigned int index, unsigned int board_size) {
     casa c;
     c.linha = index / board_size;
     c.coluna = index % board_size;
@@ -174,30 +201,59 @@ get_position_from_index(int index, int board_size) {
     return c;
 }
 
+void
+debug_print_set (struct set_t *s) {
+    for (int i = 0; i < s->elem_count; i++) {
+        casa casa_aux = get_position_from_index (s->data[i], 6);
+        printf ("(%d, %d) ", casa_aux.linha + 1, casa_aux.coluna + 1);
+    }
+    printf ("\n");
+}
+
+unsigned int
+get_smaller_element (struct set_t *s) {
+    unsigned int min_element = s->capacity + 1;
+    for (int i = 0; i < s->elem_count; i++) {
+        if (s->data[i] < min_element)
+            min_element = s->data[i];
+    }
+
+    return min_element;
+}
+
+casa
+next_candidate (struct graph_t *graph, struct set_t *c) {
+    casa candidate;
+    unsigned int first_element = get_smaller_element (c);
+
+    candidate = get_position_from_index (first_element, graph->size);
+    return candidate;
+}
+
 // Adiciona ou remove uma aresta entre duas casas.
 void
-update_edge(struct graph_t *graph, casa c1, casa c2, int operation) {
-    int v1 = get_index_from_position(graph->size, c1);
-    int v2 = get_index_from_position(graph->size, c2);
+update_edge (struct graph_t *graph, casa c1, casa c2, int operation) {
+    int v1 = get_index_from_position (graph->size, c1);
+    int v2 = get_index_from_position (graph->size, c2);
     graph->adjacency[v1][v2] = operation;
     graph->adjacency[v2][v1] = operation;
 }
 
 // Imprime a matriz de adjacência.
 void
-print_graph(struct graph_t *g) {
+print_graph (struct graph_t *g) {
     int i, j;
     int n = g->size;
 
     for (i = 0; i < BOARD_SQUARES; i++) {
         for (j = 0; j < BOARD_SQUARES; j++)
-            printf("%d ", g->adjacency[i][j]);
-        printf("\n");
+            printf ("%d ", g->adjacency[i][j]);
+        printf ("\n");
     }
 }
 
 int
-set_contains(struct set_t *s, unsigned int square_index) {
+set_contains (struct set_t *s, unsigned int square_index) {
     for (int i = 0; i < s->elem_count; i++)
         if (s->data[i] == square_index)
             return TRUE;
@@ -205,19 +261,18 @@ set_contains(struct set_t *s, unsigned int square_index) {
     return FALSE;
 }
 
-struct set_t*
-append_element(struct set_t *s, casa v) {
+struct set_t *
+append_element (struct set_t *s, casa v, unsigned int board_size) {
     if (s == NULL) {
-        fprintf(stderr, "Erro: ponteiro para conjunto não encontrado.\n");
+        fprintf (stderr, "Erro: ponteiro para conjunto não encontrado.\n");
         return NULL;
     } else if (s->capacity == s->elem_count) {
-        fprintf(stderr, "Erro: capacidade máxima do conjunto já alcançada.\n");
         return s;
     }
 
-    unsigned int square_index = get_index_from_position(s->capacity, v);
+    unsigned int square_index = get_index_from_position (board_size, v);
 
-    if (set_contains(s, square_index))
+    if (set_contains (s, square_index))
         return s;
 
     s->data[s->elem_count] = square_index;
@@ -226,21 +281,20 @@ append_element(struct set_t *s, casa v) {
     return s;
 }
 
-struct set_t*
-remove_element(struct set_t *s, casa v) {
+struct set_t *
+remove_element (struct set_t *s, casa v, unsigned int board_size) {
     if (s == NULL) {
-        fprintf(stderr, "Erro: ponteiro para conjunto não encontrado.\n");
+        fprintf (stderr, "Erro: ponteiro para conjunto não encontrado.\n");
         return NULL;
-    } else if (s->capacity == s->elem_count) {
-        fprintf(stderr, "Erro: conjunto não possui elementos.\n");
+    } else if (s->elem_count == 0) {
         return s;
     }
 
-    unsigned int square_index = get_index_from_position(s->capacity, v);
+    unsigned int square_index = get_index_from_position (board_size, v);
 
-    if (! set_contains(s, square_index))
+    if (!set_contains (s, square_index))
         return s;
-    
+
     for (int i = 0; i < s->elem_count; i++) {
         if (s->data[i] == square_index) {
             s->data[i] = s->data[s->elem_count - 1];
@@ -252,36 +306,51 @@ remove_element(struct set_t *s, casa v) {
     return s;
 }
 
-// Essa função vai adicionar de novo os vizinhos de v depois da chamada
-// recursiva.
-struct set_t*
-add_neighbours_set(struct graph_t *g, struct set_t *s, casa v) {
-    return NULL;
+struct set_t *
+add_neighbours_set (struct graph_t *g, struct set_t *s, casa c) {
+    unsigned int n = g->size;
+    unsigned int square_index = get_index_from_position (g->size, c);
+    for (unsigned int i = 0; i < n; i++) {
+        if (g->adjacency[i][square_index] == TRUE) {
+            casa neighbour = get_position_from_index (i, g->size);
+            append_element (s, neighbour, g->size);
+        }
+    }
+
+    return s;
 }
 
-// Essa função vai remover de s todos os vizinhos de v. E a casa também.
-struct set_t*
-remove_neighbours_set(struct graph_t *g, struct set_t *s, casa v) {
-    return NULL;
+struct set_t *
+remove_neighbours_set (struct graph_t *g, struct set_t *s, casa c) {
+    unsigned int n = g->size;
+    unsigned int square_index = get_index_from_position (g->size, c);
+    for (unsigned int i = 0; i < BOARD_SQUARES; i++) {
+        if (g->adjacency[i][square_index] == TRUE) {
+            casa neighbour = get_position_from_index (i, g->size);
+            remove_element (s, neighbour, g->size);
+        }
+    }
+
+    return s;
 }
 
 // Cria as arestas entre a casa que possui uma rainha e todas as casas
 // proibidas.
 void
-add_neighbours_board(struct graph_t *g, casa c) {
+add_neighbours_board (struct graph_t *g, casa c) {
     // Adiciona aresta entre todas as casas nos sentidos horizontal e vertical.
     for (int i = 0; i < g->size; i++) {
         if (i != c.coluna) {
             casa c_horizontal;
             c_horizontal.linha = i;
             c_horizontal.coluna = c.coluna;
-            update_edge(g, c, c_horizontal, ADD);
+            update_edge (g, c, c_horizontal, ADD);
         }
         if (i != c.linha) {
             casa c_vertical;
             c_vertical.linha = c.linha;
             c_vertical.coluna = i;
-            update_edge(g, c, c_vertical, ADD);
+            update_edge (g, c, c_vertical, ADD);
         }
     }
 
@@ -297,32 +366,38 @@ add_neighbours_board(struct graph_t *g, casa c) {
             casa c_diag;
             c_diag.linha = i;
             c_diag.coluna = j;
-            update_edge(g, c, c_diag, ADD);
+            update_edge (g, c, c_diag, ADD);
             i += horizontal_dir;
             j += vertical_dir;
         }
     }
 }
 
-// Função que vai realizar todo o trabalho da parte de grafos.
-unsigned int*
-find_independent_set(struct graph_t *g, struct set_t *i, struct set_t *c) {
+// Função que realiza todo o trabalho da parte de grafos.
+unsigned int *
+find_independent_set (struct graph_t *g, struct set_t *i, struct set_t *c) {
     if (i->elem_count == g->size)
         return i->data;
     else if (i->elem_count + c->elem_count < g->size)
         return NULL;
 
     casa v;
-    // remover um vértice de C
-    //  preciso ver como vou fazer tal decisão.
+    struct set_t *c_copy = copy_set (c);
+    v = next_candidate (g, c);
+    c_copy = remove_element (c_copy, v, g->size);
 
-    unsigned int *r = find_independent_set(g, append_element(i, v),
-                                           remove_neighbours_set(g, c, v));
+    unsigned int *r =
+        find_independent_set (g, append_element (i, v, g->size),
+                              remove_neighbours_set (g, c_copy, v));
 
-    i = remove_element(i, v);
-    c = add_neighbours_set(g, c, v);
+    if (!r) {
+        delete_set (c_copy);
+        c_copy = NULL;
+        i = remove_element (i, v, g->size);
+        c = remove_element (c, v, g->size);
+    }
 
-    return r ? r : find_independent_set(g, i, c);
+    return r ? r : find_independent_set (g, i, c);
 }
 
 //------------------------------------------------------------------------------
@@ -332,16 +407,23 @@ find_independent_set(struct graph_t *g, struct set_t *i, struct set_t *c) {
 //
 // n, c e r são como em rainhas_bt()
 
-unsigned int*
-rainhas_ci(unsigned int n, unsigned int k, casa *c, unsigned int *r) {
-    struct graph_t* board = create_graph(n);;
-    struct set_t* independent_set = create_set(n);;
-    struct set_t* candidates_set = create_set(BOARD_SQUARES);;
+unsigned int *
+rainhas_ci (unsigned int n, unsigned int k, casa *c, unsigned int *r) {
+    struct graph_t *board = create_graph (n);
+    struct set_t *independent_set = create_set (n);
+    struct set_t *candidates_set = create_set (BOARD_SQUARES);
 
-    for(int i = 0; i < candidates_set->capacity; i++) {
+    for (int i = 0; i < candidates_set->capacity; i++) {
         candidates_set->data[i] = i;
-        casa vertice = get_position_from_index(i, board->size);
-        //add_queen_neighbours(board, vertice);
+        candidates_set->elem_count++;
+        casa vertice = get_position_from_index (i, board->size);
+        add_neighbours_board (board, vertice);
+    }
+
+    find_independent_set (board, independent_set, candidates_set);
+    for (int i = 0; i < n; i++) {
+        casa aux = get_position_from_index (independent_set->data[i], n);
+        r[i] = aux.coluna + 1;
     }
 
     return r;
